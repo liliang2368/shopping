de00d1e77b3f9daaa08ee0c6fb88f494f611250c
liyang <2363582258@qq.com>
2020-09-30 11:55:58 +0800

    引入 swagger-ui

--- src/main/java/com/ly/shopping/OrderShoppingApplication.java
+++ src/main/java/com/ly/shopping/OrderShoppingApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @MapperScan("com.ly.shopping.dao")
 public class OrderShoppingApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OrderShoppingApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/ly/shopping/config/AppConfig.java
@@ -0,0 +1,26 @@
+package com.ly.shopping.config;
+
+//import redis.clients.jedis.Jedis;
+//
+//
+//@Configuration
+//@ComponentScan(basePackages = "com.ly")
+//@EnableTransactionManagement // 查找spring 托管Bean 上是否有
+//// @Transactional注解,如果有，则启用事务管理器
+//@PropertySource("classpath:/properties/redis.properties")
+//public class AppConfig {
+////	private Logger log = Logger
+//	@Value("${redis.hostName}")
+//	private String host;
+//	@Value("${redis.port}")
+//	private Integer port;
+//
+//	//将返回值的结果交给spring容器进行管理,如果以后想要使用该对象则可以直接注入.
+//	@Bean
+//	public Jedis jedis() {
+//
+//		return new Jedis(host, port);
+//	}
+//
+//
+//}
